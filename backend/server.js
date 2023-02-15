ddf5a8d743120b03a600aac540797c5b275e2e9c
Bimal27 <bimalsah1996@gmail.com>
2023-02-15 23:24:58 +0100

    updated error

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import listEndpoints from 'express-list-endpoints'
-import cookieParser from 'cookie-parser'
-import cloudinary from 'cloudinary'
-import bodyParser from 'body-parser'
-import fileUpload from 'express-fileupload'
+import express from "express";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
-import cors from 'cors'
-import productRouter from './routes/product.js'
-import errorHandler from './middleware/error.js'
+import cors from "cors";
+import productRouter from "./routes/product.js";
+import errorHandler from "./middleware/error.js";
 import {
   badRequestErrorHandler,
   notFoundErrorHandler,
   forbiddenErrorHandler,
-  genericServerErrorHandler,
-} from './errorHandlers.js'
+  genericServerErrorHandler
+} from "./errorHandlers.js";
 
-import userRouter from './routes/user.js'
-import reviewRouter from './routes/review.js'
-import orderRouter from './routes/order.js'
-import paymentRouter from './routes/payment.js'
+import userRouter from "./routes/user.js";
+import reviewRouter from "./routes/review.js";
+import orderRouter from "./routes/order.js";
+import paymentRouter from "./routes/payment.js";
 
-const server = express()
+const server = express();
 // import path from 'path'
 
 // const __dirname = path.resolve();
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 // ************************* MIDDLEWARES ********************************
 
@@ -44,7 +44,6 @@ const port = process.env.PORT || 3001
 //   credentials: true
 // };
 
-
 // const corsOptions = {
 //   //To allow requests from client
 //   origin: [
@@ -56,7 +55,6 @@ const port = process.env.PORT || 3001
 //   exposedHeaders: ["set-cookie"]
 // };
 
-
 server.use(
   cors({
     origin: "https://strivazon-store-ecommerce.vercel.app",
@@ -65,26 +63,24 @@ server.use(
 );
 server.use(cookieParser());
 
-server.use(express.json())
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(fileUpload())
-
-
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(fileUpload());
 
 // ************************* ERROR MIDDLEWARES ********************************
-server.use(errorHandler)
-server.use(badRequestErrorHandler)
-server.use(notFoundErrorHandler)
-server.use(forbiddenErrorHandler)
-server.use(genericServerErrorHandler)
+server.use(errorHandler);
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(genericServerErrorHandler);
 
 // ************************* Routes *******************************
-server.use('/', productRouter)
-server.use('/', reviewRouter)
+server.use("/", productRouter);
+server.use("/", reviewRouter);
 server.use("/register", userRouter);
-server.use('/', userRouter)
-server.use('/', orderRouter)
-server.use('/', paymentRouter)
+server.use("/", userRouter);
+server.use("/", orderRouter);
+server.use("/", paymentRouter);
 
 // server.use(express.static(path.join(__dirname, "../frontend/build")));
 
@@ -93,24 +89,24 @@ server.use('/', paymentRouter)
 // });
 
 // ************************* Mongo Connection *******************************
-mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connect(process.env.MONGO_CONNECTION);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-mongoose.connection.on('connected', () => {
-  console.log('Successfully connected to Mongo!')
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
   server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log(`Server running on port ${port}`)
-  })
-})
-mongoose.connection.on('error', () => {
-  console.log(err)
-})
+    console.table(listEndpoints(server));
+    console.log(`Server running on port ${port}`);
+  });
+});
+// mongoose.connection.on('error', () => {
+//   console.log(err)
+// })
 
 // **************** Unhandled Promise Rejection *******************
 
